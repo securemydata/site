7c37dab6226a9a6954264bc422f430f7ad9324e2
guillaume.regairaz <(tubrgz)>
2019-05-03 11:22:41 +0200

    user provider

---
+++ src/AppBundle/Security/User/WebserviceUserProvider.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guillaume.regairaz
+ * Date: 03/05/2019
+ * Time: 11:15
+ */
+// src/AppBundle/Security/User/WebserviceUserProvider.php
+namespace AppBundle\Security\User;
+
+use AppBundle\Security\User\WebserviceUser;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class WebserviceUserProvider implements UserProviderInterface
+{
+    public function loadUserByUsername($username)
+    {
+        return $this->fetchUser($username);
+    }
+
+    public function refreshUser(UserInterface $user)
+    {
+        if (!$user instanceof WebserviceUser) {
+            throw new UnsupportedUserException(
+                sprintf('Instances of "%s" are not supported.', get_class($user))
+            );
+        }
+
+        $username = $user->getUsername();
+
+        return $this->fetchUser($username);
+    }
+
+    public function supportsClass($class)
+    {
+        return WebserviceUser::class === $class;
+    }
+
+    private function fetchUser($username)
+    {
+        // make a call to your webservice here
+        $userData = array();
+        // pretend it returns an array on success, false if there is no user
+
+        if ($userData) {
+            $password   = 'ok';
+            $username   = 'rgz';
+            $salt       = 123456786415212;
+            $roles      = ROLE_ADMIN;
+
+            // ...
+
+            return new WebserviceUser($username, $password, $salt, $roles);
+        }
+
+        throw new UsernameNotFoundException(
+            sprintf('Username "%s" does not exist.', $username)
+        );
+    }
+}
